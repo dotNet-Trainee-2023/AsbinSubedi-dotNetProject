8fc24ee05496c18b9af18b54e7cace63f361e8ae
Subedi <asbin.subedi@cotiviti.com>
2023-09-18 22:44:36 +0545

    Assignment day-2

---
+++ Assignment2/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class Employee
+    {
+        public String FirstName  { get; set ;}
+        public String LastName { get; set ;} 
+        public string Email { get; set ;}   
+        public string Phone { get; set ;}
+        public string Gender { get; set ;}
+        public int Age { get; set; }
+        public string JobTitle { get; set; }
+        public int Years { get; set; }
+        public double Salary { get; set; }
+        public string Department { get; set; }
+    }
+
+
+}
+
---
+++ Assignment2/Program.cs
@@ -0,0 +1,94 @@
+﻿using Assignment2;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EmployeeManagement
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Employee> employees = ReadEmployeeDataFromCSV("D:\\employees.csv");
+
+            var employeesByDepartment = employees.GroupBy(e => e.Department);
+
+            foreach(var departmentalGroup in employeesByDepartment)
+            {
+                Console.WriteLine($" Department : {departmentalGroup.Key}");
+
+                foreach(var employee in departmentalGroup)
+                {
+                    Console.WriteLine($"-{employee.FirstName} {employee.LastName}");
+                }
+            }
+
+            var highestPaidProjectManager = employees
+                .Where(e => e.JobTitle == "Project Manager")
+                .OrderByDescending(e => e.Salary)
+                .FirstOrDefault();
+            Console.WriteLine($"The Highest Paid Project manager is {highestPaidProjectManager.FirstName}");
+
+            var mostExperiencedWebDeveloper = employees
+                .Where(e => e.JobTitle == "Web Developer")
+                .OrderByDescending(e => e.Years)
+                .FirstOrDefault();
+
+            Console.WriteLine($"The most Experiedn Web developer is : {mostExperiencedWebDeveloper.FirstName}");
+
+            var averageSalaryByJobTitle = employees
+                .GroupBy(e => e.JobTitle)
+                .Select(group => new
+                {
+                    JobTitle = group.Key,
+                    AverageSalary = group.Average(e => e.Salary)
+
+                });
+            foreach ( var avgSalary in averageSalaryByJobTitle )
+            {
+                Console.WriteLine($"Average Salary of {avgSalary.JobTitle} is {avgSalary.AverageSalary
+                    }");
+            }
+
+            var genderCounts = employees
+                .GroupBy(e => e.Gender)
+                .Select(group => new
+                {
+                    Gender = group.Key,
+                    Count = group.Count()
+                });
+           foreach(var genderCount in genderCounts)
+            {
+                Console.WriteLine($"Number of {genderCount.Gender} Emplloyee is {genderCount.Count}");
+            }
+        }
+
+
+        static List<Employee> ReadEmployeeDataFromCSV(string filePath)
+        {
+            
+            List<Employee> employees = File.ReadLines(filePath)
+                .Skip(1) 
+                .Select(line => line.Split(','))
+                .Select(data => new Employee
+                {
+                    FirstName = data[0],
+                    LastName = data[1],
+                    Email = data[2],
+                    Phone = data[3],
+                    Gender = data[4],
+                    Age = int.Parse(data[5]),
+                    JobTitle = data[6],
+                    Years = int.Parse(data[7]),
+                    Salary = double.Parse(data[8]),
+                    Department = data[9]
+                })
+                .ToList();
+
+
+            return employees;
+        }
+    }
+}
