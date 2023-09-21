f9795dae59523793b48a6c98f9c5db430ebce730
Subedi <asbin.subedi@cotiviti.com>
2023-09-21 10:05:39 +0545

    assignment 3

---
+++ Assignment3/Data/MovieDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment3.Models;
+
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment3.Models.Movies> Movies { get; set; }/* = default!;*/
+    }
---
+++ Assignment3/Models/Movies.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment3.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Title of Movie is required")]
+        public string Title { get; set; }
+        [Required(ErrorMessage ="Enter the release Date.")]
+        [DataType(DataType.Date)]
+        public DateTime releaseDate { get; set; }
+
+        [Required(ErrorMessage ="Genre is required")]
+        public string genre { get; set; }
+
+        [Range(1,10,ErrorMessage ="Rating must be between 1 and 10")]
+        public decimal rating { get; set; }
+
+    }
+}
